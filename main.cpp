26be787e557632a6e6fc4e0eb8731384dc28eafd
Jake <jsgildea@mail.csuchico.edu>
2020-09-09 14:25:48 -0700

    updated error

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main()
     }
   }while( input != 'q' && input != 'Q' );
 
-  if(list.size >= 1){
+  if(list.size() >= 1){
      for(int i = 0; i < list.size(); i++){
          cout << i << " " << list[i] << endl;
      {
